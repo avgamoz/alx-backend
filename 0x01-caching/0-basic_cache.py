ccbfadb865175124d1763ee72b367cc7507f88b4
AKINSOLA BABAJIDE AMOS <90686696+avgamoz@users.noreply.github.com>
2023-07-25 21:20:45 +0100

    0x01-caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" 0. Basic dictionary
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ a class that inherits from BaseCaching and is
+        a caching system.
+        Attributes:
+            key, item
+        Methods:
+            put(), get()
+    """
+
+    def _init_(self):
+        super()._init_()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Assign key and item to the cache system """
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get the value from the cache with key"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
